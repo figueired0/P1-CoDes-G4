0b97946e0303acfbc47d240fcefd3f4444aa33c5
beatrizbernardino <pbeatriz18@hotmail.com.br>
2019-09-11 18:36:46 -0300

    javascript

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let db = coDesConnect('https://guia-para-um-engenheiro.firebaseio.com/')
+
+
+  db.download('/', function(data) {
+    context = data
+    console.log(context)
+    coDesReplace('.submenu', context)
+    coDesReplace('.categoriasproj', context)
+
+
+  })
+
+
+})
