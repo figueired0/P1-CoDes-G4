6f345204d98b0b8dc12f660f0c8aaf1f8e37c5a1
Julia <julia.r.martins@hotmail.com>
2019-09-13 18:24:26 -0300

    js

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   let db = coDesConnect('https://guia-para-um-engenheiro.firebaseio.com/')
 
+  var url_str= window.location.href;
+  
 
+  var url=new URL(url_str);
+  var c = url.searchParams.get("key");
+  var p = url.searchParams.get("key");
+  alert(p);
   db.download('/', function(data) {
     context = data
     console.log(context)
     coDesReplace('.submenu', context)
-    coDesReplace('.categoriasproj', context)
+    coDesReplace('.categoria', data["portfolio"][c])
+    coDesReplace('.quadrado', data["portfolio"][c])
+
 
 
   })
